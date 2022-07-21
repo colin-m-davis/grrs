096d7f79f4c3fe0d764ce2e6f0c0e7b4b0d3e311
colindavis <colinmichaelsdavis@gmail.com>
2022-07-20 22:35:27 -0700

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path)
+        .expect("could not read file");
+    
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
